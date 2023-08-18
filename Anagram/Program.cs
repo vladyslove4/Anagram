0b3456de18d25943a26f844322c6261be11e9f8a
Vladyslav Kasianov <kasyanov.3d@gmail.com>
2023-08-18 18:41:54 +0300

    -add solution

---
+++ Anagram/Program.cs
@@ -0,0 +1,25 @@
+﻿using Anagram.Services;
+
+namespace Anagram;
+class Program
+{
+    static void Main(string[] args)
+    {
+        IReverter _reverter = new ReverseTextService();
+        while (true)
+        {
+            DoWork(_reverter);
+            Console.WriteLine();
+        }
+    }
+
+    private static void DoWork(IReverter reverter)
+    {
+        Console.WriteLine("Type here");
+        string? data = Console.ReadLine();
+        if (data != null)
+        {
+            Console.WriteLine(reverter.ReverseText(data));
+        }
+    }
+}
---
+++ Anagram/Services/ReverseTextService.cs
@@ -0,0 +1,40 @@
+﻿namespace Anagram.Services;
+
+internal class ReverseTextService : IReverter
+{
+    public string ReverseText(string data)
+    {
+        if (data == null)
+        {
+            throw new ArgumentNullException(nameof(data));
+        }
+        string[] words = data.Split(' ');
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (words[i].Length > 1)
+            {
+                words[i] = ReverseWord(words[i]);
+            }
+        }
+
+        return string.Join(' ', words);
+    }
+
+    private string ReverseWord(string data)
+    {
+        char[] chars = data.ToCharArray();
+        int j = data.Length - 1;
+        for (int i = 0; i < data.Length; i++)
+        {
+            if (char.IsLetter(data[i]))
+            {
+                while (!char.IsLetter(data[j]))
+                    j--;
+                chars[j] = data[i];
+                j--;
+            }
+        }
+
+        return new string(chars);
+    }
+}
